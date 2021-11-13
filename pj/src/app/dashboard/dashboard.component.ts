362db35847cdadfc8552585967aeae0ae44e234d
Chriiistian <christian.hernandez.aranda@alumnos.uta.cl>
2021-11-13 00:06:17 -0300

    Update 1.9
    Actualización final proyecto 2

--- pj/src/app/dashboard/dashboard.component.ts
+++ pj/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DashboardService , Venta, Comentario, Chat} from '../servicios/dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,8 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  ventas:Venta[]=[];
+  coment:Comentario[]=[];
+  chats:Chat[]=[];
+  
+  constructor(private _dashboardService: DashboardService) {
+    console.log("creando el componente dashboard..")
+    console.log(_dashboardService.getVentas());
+    this.ventas=_dashboardService.getVentas();
+    console.log(this.ventas[0].nombre);
 
-  constructor() { }
+    console.log(_dashboardService.getComentarios());
+    this.coment=_dashboardService.getComentarios();
+    console.log(this.coment[0].nombre);
+
+    console.log(_dashboardService.getChats());
+    this.chats=_dashboardService.getChats();
+    console.log(this.chats[0].nombre);
+   }
 
   ngOnInit(): void {
   }
